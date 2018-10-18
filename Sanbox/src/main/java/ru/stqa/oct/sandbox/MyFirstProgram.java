fc696709a453691ac5608abff823c0cd657a7c51
Nikonova <39440345+Nikonova@users.noreply.github.com>
2018-10-18 12:28:21 -0400

    Расчет "Расстояние между двумя точками"

--- Sanbox/src/main/java/ru/stqa/oct/sandbox/MyFirstProgram.java
+++ Sanbox/src/main/java/ru/stqa/oct/sandbox/MyFirstProgram.java
@@ -8,23 +8,19 @@ public class MyFirstProgram {
        hello("user");
        hello("Svetlana");
 
-       double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+       Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-       double a = 4;
-       double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+       Rectangle r = new Rectangle(4, 6 );
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+       Point d = new Point(5,5,4,4);
+        System.out.println("Расстояние между точками " + " A(" + d.x1 + "," + d.y1 + ")" + " и " + " B(" + d.x2 +"," + d.y2 + ")"+ " = " + d.area());
     }
 
     public static void hello(String somebody) {
                 System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
 
-    public static double area(double a, double b) {
-        return a * b;
     }
-}
